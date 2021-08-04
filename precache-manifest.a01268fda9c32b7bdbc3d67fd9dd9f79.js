063a8d7f6f34a57062fe2caa5cb9b78931dbab89
1998mohamed <mohamedabdulhamed98@gmail.com>
2021-08-04 20:43:31 +0200

    Updates

---
+++ precache-manifest.a01268fda9c32b7bdbc3d67fd9dd9f79.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f3034c613e276483b4c09412cd1f6b",
+    "url": "/mo-suit/index.html"
+  },
+  {
+    "revision": "15335e48bfeba63eef64",
+    "url": "/mo-suit/static/css/main.bd61a861.chunk.css"
+  },
+  {
+    "revision": "74114ab35f526a0561d4",
+    "url": "/mo-suit/static/js/2.82b1f7d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mo-suit/static/js/2.82b1f7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15335e48bfeba63eef64",
+    "url": "/mo-suit/static/js/main.b37e845d.chunk.js"
+  },
+  {
+    "revision": "d0eec9d22dc7c0a1cfe5",
+    "url": "/mo-suit/static/js/runtime-main.49ea063e.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/mo-suit/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "f2eef21ed622d5bf4804f1b60c01920b",
+    "url": "/mo-suit/static/media/men1.f2eef21e.jpg"
+  },
+  {
+    "revision": "300e7cb2df469063ad647d5f3c0aa2e8",
+    "url": "/mo-suit/static/media/men10.300e7cb2.jpg"
+  },
+  {
+    "revision": "e808e267c6cd9eec07762ad795e9889e",
+    "url": "/mo-suit/static/media/men2.e808e267.jpg"
+  },
+  {
+    "revision": "53d871d064f39a0d5483397c0bef942e",
+    "url": "/mo-suit/static/media/men3.53d871d0.jpg"
+  },
+  {
+    "revision": "f36b45325135886c58be15b160e597ba",
+    "url": "/mo-suit/static/media/men4.f36b4532.jpg"
+  },
+  {
+    "revision": "3480ad0dc59b08e945e7361906a9b2a1",
+    "url": "/mo-suit/static/media/men5.3480ad0d.jpg"
+  },
+  {
+    "revision": "c8e1cd79a25322bc8c30955f1b030e6c",
+    "url": "/mo-suit/static/media/men6.c8e1cd79.jpg"
+  },
+  {
+    "revision": "2e312aa436153c1dbd7bb08539e75faa",
+    "url": "/mo-suit/static/media/men7.2e312aa4.jpg"
+  },
+  {
+    "revision": "116700af509a8533aea4d494d5ba78b8",
+    "url": "/mo-suit/static/media/men8.116700af.jpg"
+  },
+  {
+    "revision": "4a8fd1a659e1c2aa710b0467d641f793",
+    "url": "/mo-suit/static/media/men9.4a8fd1a6.jpg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/mo-suit/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/mo-suit/static/media/times-solid.2c5552a1.svg"
+  }
+]);
